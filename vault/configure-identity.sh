3b3fb41b6a545e69f25ff1c0b31d76657d18aae0
Ric Featherstone <ricfeatherstone@users.noreply.github.com>
2021-10-26 13:40:11 +0100

    fix: remove spiffe prefix from entity to make it readable

--- vault/configure-identity.sh
+++ vault/configure-identity.sh
@@ -4,9 +4,10 @@ export VAULT_SKIP_VERIFY=true
 export VAULT_ADDR=https://localhost:30000
 export VAULT_TOKEN=$(cat init.json | jq -r .root_token)
 
+CERT_ACCESSOR=$(vault auth list -format=json | jq -r '."cert/".accessor')
+
 for i in svc-a svc-b; do
-  ID=$(vault write -field=id identity/entity name=spiffe://controlplane.io/workload/$i policies=$i)
-  CERT_ACCESSOR=$(vault auth list -format=json | jq -r '."cert/".accessor')
+  ID=$(vault write -field=id identity/entity name=controlplane.io/workload/$i policies=$i)
   vault write identity/entity-alias name="$i.controlplane.io" \
     canonical_id=$ID \
     mount_accessor=$CERT_ACCESSOR
