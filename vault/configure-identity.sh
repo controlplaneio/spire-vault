5663f4462d9808695d51c710a34ebea6d8a4af94
Ric Featherstone <ricfeatherstone@users.noreply.github.com>
2021-10-26 10:40:25 +0100

    refactor: alter workload spiffe ids

--- vault/configure-identity.sh
+++ vault/configure-identity.sh
@@ -5,7 +5,7 @@ export VAULT_ADDR=https://localhost:30000
 export VAULT_TOKEN=$(cat init.json | jq -r .root_token)
 
 for i in svc-a svc-b; do
-  ID=$(vault write -field=id identity/entity name=spiffe://controlplane.io/$i policies=$i)
+  ID=$(vault write -field=id identity/entity name=spiffe://controlplane.io/workload/$i policies=$i)
   CERT_ACCESSOR=$(vault auth list -format=json | jq -r '."cert/".accessor')
   vault write identity/entity-alias name="$i.controlplane.io" \
     canonical_id=$ID \
